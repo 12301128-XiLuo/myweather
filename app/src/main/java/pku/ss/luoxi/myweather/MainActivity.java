f66813be43744e17213487e3dac2bc80e65c9b82
12301128-XiLuo <332810362@qq.com>
2016-09-20 21:30:48 +0800

    chapter 1

---
+++ app/src/main/java/pku/ss/luoxi/myweather/MainActivity.java
@@ -0,0 +1,15 @@
+package pku.ss.luoxi.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by admin on 2016/9/20.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
