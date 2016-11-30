1daae7f2ae2348c7cef0cc29deb3829ead806a9d
12301128-XiLuo <332810362@qq.com>
2016-11-30 14:07:52 +0800

    导航页

---
+++ app/src/main/java/pku/ss/luoxi/myweather/Guide.java
@@ -0,0 +1,85 @@
+package pku.ss.luoxi.myweather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guide extends Activity implements ViewPager.OnPageChangeListener{
+    private ViewPagerAdapter vpAdapter;
+    private ViewPager vp;
+    private List<View> views;
+
+    private ImageView[] dots;
+    private int[] ids = {R.id.iv1,R.id.iv2,R.id.iv3};
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        initViews();
+        initDots();
+        btn = (Button) views.get(2).findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Guide.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    //初始化小圆点
+    private void initDots(){
+        dots = new ImageView[views.size()];
+        for(int i=0;i<views.size();i++){
+            dots[i] = (ImageView) findViewById(ids[i]);
+        }
+    }
+
+    //初始化页面
+    private void initViews(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+        views = new ArrayList<View>();
+        views.add(inflater.inflate(R.layout.page1,null));
+        views.add(inflater.inflate(R.layout.page2,null));
+        views.add(inflater.inflate(R.layout.page3,null));
+        vpAdapter = new ViewPagerAdapter(views,this);
+        vp = (ViewPager) findViewById(R.id.viewpager);
+        vp.setAdapter(vpAdapter);
+        vp.setOnPageChangeListener(this);
+    }
+
+    //重写导航换页事件监听
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        for(int i=0;i<ids.length;i++){
+            if(i==position){
+                dots[i].setImageResource(R.drawable.page_indicator_focused);
+            }else {
+                dots[i].setImageResource(R.drawable.page_indicator_unfocused);
+            }
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+}
